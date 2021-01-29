2cc38fe367a9f7034e7329bb6c5ce1cce8df14f9
PavelSkachok <pvlsk30@gmail.com>
2021-01-29 06:57:28 +0300

    additional apiKey

--- newsApp/Model/NewsService.swift
+++ newsApp/Model/NewsService.swift
@@ -27,9 +27,12 @@ class NewsService {
             "q": keyword,
             "country": endpoint == .topHeadlines ? countryEndpoint: nil,
             "category":endpoint == .topHeadlines ? stringEndpoints: nil,
-//            "apiKey": "b2a849c07c30432eb9e293ee88c2117c",
-//            "apiKey": "751b73924a1a42b88d7ba11f1b04ed3b",
-            "apiKey": "7cfca023b0f7404bb6474a2797e7dfc0",
+//          "apiKey": "b2a849c07c30432eb9e293ee88c2117c",
+//          "apiKey": "7cfca023b0f7404bb6474a2797e7dfc0",
+            "apiKey": "751b73924a1a42b88d7ba11f1b04ed3b",
+//            "apiKey": "3a807e689d1444b3a1aab06e68f5ceb8",
+//            "apiKey": "69e092f2ff5544809589426108a11c77",
+//            "apiKey": "f032808cd7274be987d2306bbdeadc0b",
         ]
         .compactMap {
             URLQueryItem(name: $0.key, value: $0.value)
